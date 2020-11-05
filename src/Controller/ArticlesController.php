101211d0c7ced95390e4e72dbe5959611a0cf74d
Khany80 <emilie.gense@gmail.com>
2020-11-05 20:18:11 +0100

    création des premieres pages'

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use App\Repository\ArticlesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function index(Request $request, PaginatorInterface $paginator)
+    {
+        $donnees = $this->getDoctrine()->getRepository(Articles::class)->findBy([],
+    ['created_at' => 'desc']);
+        $articles = $paginator->paginate(
+            $donnees,
+            $request->query->getInt('page', 1),
+            4
+        );
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article")
+     */
+    public function article($id)
+    {
+       $article = $this->getDoctrine()->getRepository(Articles::class)->findOneBy(['id' => $id]);
+        
+       return $this->render('articles/article.html.twig', compact('article'));
+    }
+}
